ad5f8960b978b5a4b8b5d8eb380974fcb530fa5e
Masoud <masoud.zeinalabedini@gmail.com>
2023-04-30 17:56:55 +0330

    Add GradiantContainer widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,27 +5,32 @@ void main() {
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.deepPurple,
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 26, 2, 80),
-                Color.fromARGB(255, 45, 7, 98),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'This is my first app',
-              style: TextStyle(color: Colors.white, fontSize: 28),
-            ),
-          ),
-        ),
+        body: GradiantContainer(),
       ),
     ),
   );
 }
 
-
+class GradiantContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 45, 7, 98),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'This is my first app',
+          style: TextStyle(color: Colors.white, fontSize: 28),
+        ),
+      ),
+    );
+  }
+}
