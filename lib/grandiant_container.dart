88fee5622f916cf6151da8ebf794f68673b9304a
Masoud <masoud.zeinalabedini@gmail.com>
2023-04-30 18:15:02 +0330

    Extract GreadiantContainer widget to its own file

---
+++ lib/grandiant_container.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class GradiantContainer extends StatelessWidget {
+  const GradiantContainer({super.key});
+  @override 
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 45, 7, 98),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'This is my first app',
+          style: TextStyle(color: Colors.white, fontSize: 28),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'package:first_learning_app/grandiant_container.dart';
+
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.deepPurple,
         body: GradiantContainer(),
       ),
     ),
   );
-}
-
-class GradiantContainer extends StatelessWidget {
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 45, 7, 98),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          'This is my first app',
-          style: TextStyle(color: Colors.white, fontSize: 28),
-        ),
-      ),
-    );
-  }
 }
