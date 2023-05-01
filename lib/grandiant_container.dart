1c268c138185ec52fcc85f2a38062048e27dab8d
Masoud <masoud.zeinalabedini@gmail.com>
2023-05-01 09:49:45 +0330

    Set all virabales to const in GradientContaier

--- lib/grandiant_container.dart
+++ lib/grandiant_container.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 
 import 'package:first_learning_app/styled_text.dart';
 
-Alignment startAlignment = Alignment.topLeft;
-Alignment endAlignment = Alignment.bottomRight;
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
 
 class GradiantContainer extends StatelessWidget {
   const GradiantContainer({super.key});
   @override
   Widget build(context) {
     return Container(
-      decoration: BoxDecoration(
+      decoration: const BoxDecoration(
         gradient: LinearGradient(
-          colors: const [
+          colors: [
             Color.fromARGB(255, 26, 2, 80),
             Color.fromARGB(255, 45, 7, 98),
           ],
